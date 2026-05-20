54c95e429d3b22aeb8c71d5de38b0db66cec1e73
oriana02 <or.solorzano@duocuc.cl>
2026-05-19 20:06:33 -0400

    eva2

--- server.js
+++ server.js
@@ -54,7 +54,13 @@ app.get("/api/usuarios", (req, res) => {
       res.status(500).json({ error: "Error al obtener usuarios" });
       return;
     }
-    res.json(results);
+    res.json(
+      results.map((user) => ({
+        ...user,
+
+        nombre: user.nombre ? user.nombre.slice(0, 5) : "",
+      })),
+    );
   });
 });
 
